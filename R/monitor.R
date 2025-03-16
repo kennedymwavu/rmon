1934e63e98f7b3000f2baa0d80225a061e5dcedc
Kennedy Mwavu <mwavukennedy@gmail.com>
2025-03-16 18:41:34 +0300

    ch: run examples in `interactive()` mode only

--- R/monitor.R
+++ R/monitor.R
@@ -37,7 +37,7 @@
 #'
 #' The function runs indefinitely until interrupted.
 #' @examples
-#' \dontrun{
+#' if (interactive()) {
 #' # monitor current directory, rerun 'app.R' on changes, ignore 'dev.R' and
 #' # any files in 'test/' directory:
 #' rmon::monitor(
@@ -57,14 +57,15 @@
 #' @return `NULL`
 #' @export
 monitor <- function(
-    dir = ".",
-    file,
-    ext = "*",
-    monitor_hidden = FALSE,
-    exclude_files = NULL,
-    exclude_patterns = NULL,
-    exclude_dirs = NULL,
-    delay = 1) {
+  dir = ".",
+  file,
+  ext = "*",
+  monitor_hidden = FALSE,
+  exclude_files = NULL,
+  exclude_patterns = NULL,
+  exclude_dirs = NULL,
+  delay = 1
+) {
   file <- normalizePath(
     path = file.path(dir[[1]], file[[1]]),
     mustWork = TRUE
@@ -142,11 +143,10 @@ monitor <- function(
 #' @details Retrieves current system date and time, formatted in
 #' a human-readable way.
 #' @examples
-#' \dontrun{
 #' current_time()
-#' }
 #' @return String in the format "YYYY-MM-DD H:M:S" with the
 #' timezone appended at the end.
+#' @keywords internal
 #' @noRd
 current_time <- function() {
   format(x = Sys.time(), format = "%F %T", usetz = TRUE)
@@ -156,10 +156,9 @@ current_time <- function() {
 #'
 #' @param type String. Type of message to show. Either "restarting"(default) or "starting".
 #' @examples
-#' \dontrun{
 #' dash_and_msg()
-#' }
 #' @return `NULL`
+#' @keywords internal
 #' @noRd
 dash_and_msg <- function(type = c("restarting", "starting")) {
   type <- match.arg(arg = type)
